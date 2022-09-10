ed5cd9569a72817ae3322d699da25d98e432ca97
lika <tagatalelika@gmail.com>
2022-09-10 17:04:55 +1100

    projet

---
+++ app/Controllers/Http/SecurityController.ts
@@ -0,0 +1,48 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
+
+export default class SecurityController {
+
+    async getLogin({view}: HttpContextContract){
+        return view.render('vue/login');
+    }
+
+    async postLogin({request, auth, response, session}: HttpContextContract){
+       const email = request.input('email')
+       const password = request.input('password')
+
+       try{
+            await auth.use('web').attempt(email, password)
+            session.flash({success: "Vous vous êtes bien connecté"})
+            return response.redirect('/')
+       }catch {
+            session.flash({error: "Identifiant incorrect"})
+            response.redirect().toRoute('login')
+       }
+    }
+
+    async logout({auth, response}: HttpContextContract){
+        await auth.use('web').logout()
+        response.redirect().toRoute('login')
+    }
+
+    async getInscription({view}: HttpContextContract){
+        return view.render('vue/inscription');
+    }
+
+    async postInscription({request, response, session}: HttpContextContract){
+        
+        const user = await User.findByOrFail('email', request.input('email'))
+        if(user.id == null){
+            const utilisateur = new User()
+            utilisateur.email = request.input('email')
+            utilisateur.password = request.input('password')
+            await utilisateur.save()
+            session.flash({success: "Vous vous êtes bien inscrit"})
+            response.redirect().toRoute('home')
+        }else{
+            session.flash({error: "Email déja utilisé, veuillez en choisir un autre !"})
+            response.redirect().toRoute('inscription')
+        }
+     }
+}
